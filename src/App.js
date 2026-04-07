859821571825d788c8cd5fe91ecfabeef91da59c
= <avula.usedu1@gmail.com>
2026-04-07 18:27:51 -0500

    changes in the code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import Header from './features/header/Header'
+import Footer from './features/footer/Footer'
+//  
+import TodoList from './features/todos/TodoList'
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <section>
-          <h1>Redux Fundamentals Example</h1>
-
-          <div className="navContent">
-            <div className="navLinks"></div>
-          </div>
+      <nav></nav>
+      <main>
+        <section className="medium-container">
+          <h2>Todos</h2>
+          <div className="todoapp">
+            <Header />
+            <TodoList />
+            <Footer />
+          </div>{' '}
         </section>
-      </nav>
-      <section>
-        <h2>Welcome to the Redux Fundamentals example app!</h2>
-      </section>
+      </main>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-
+import { Provider } from "react-redux";
 import './api/server'
-
+import store from './store';
+import { fetchTodos } from './features/todos/todosSlice';
+store.dispatch(fetchTodos())
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}></Provider>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
